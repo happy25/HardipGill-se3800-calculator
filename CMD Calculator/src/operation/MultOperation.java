69f219b6fc593665b26eb295dc50b50ecc81a3c5
Hardip Gill <gillh@msoe.edu>
2015-12-23 21:03:57 -0600

    [User Story 3] Added Multiplication Operation class

---
+++ CMD Calculator/src/operation/MultOperation.java
@@ -0,0 +1,33 @@
+package operation;
+
+import computation.Computation;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by gillh on 12/20/2015.
+ *
+ * This class is equipped to handle product of numbers
+ */
+public class MultOperation implements Operation {
+
+    /**
+     * This method will take a list of numbers and return a computation object of the result
+     * @param numbers list of numbers to add
+     * @return Computation object
+     */
+    @Override
+    public Computation calc(List<Integer> numbers) throws Exception {
+        float product = 1;
+        if(numbers.size()>0){                   // if list is not empty
+            for(Integer num: numbers){              // for every number in list
+                product *= num;                         // multiply by number
+            }
+            return new Computation("ADD", Arrays.toString(numbers.toArray()),Float.toString(product));
+        }
+        else{
+            throw new IllegalArgumentException("Number list must contain at least one number");
+        }
+    }
+}
